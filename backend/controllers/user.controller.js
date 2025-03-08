e6e1347de584d5f893f791e5abefa99acfcf5dc7
sagar252004 <132927520+sagar252004@users.noreply.github.com>
2025-03-08 16:26:56 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -150,10 +150,16 @@ export const login = async(req, res) => {
 
 
 export const logout = async(req, res) => {
-    try {
-        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
-            message: "Logged out successfully.",
-            success: true
+     try {
+        return res.status(200).cookie("token", "",{
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+        expires: new Date(0),
+
+        }).json({
+            message:( "Logged out successfully."),
+            success: true,
         })
     } catch (error) {
         console.log(error);
